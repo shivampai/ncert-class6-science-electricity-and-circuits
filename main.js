5202e11374cd713f273c019b0d5ea12fffbc9ef8
Shivam Pai <72920537+shivampai@users.noreply.github.com>
2021-02-27 10:41:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,4 +20,36 @@ function ebookNext() {
 }
 function ebookDownload() {
   window.open("https://shivampai.github.io/ncert-class6-science-electricity-and-circuits/NCERT-Books-for-class-6-Science-Chapter-12.pdf");
+};
+var points = 0;
+var wrong = [""]
+function checkans() {
+  if(document.getElementById("I_one").value == "Switch" || "switch"){
+    points = points + 1;
+  } else {
+    wrong.push("I question 1 Wrong . Correct Ans: Switch");
+  }
+  if(document.getElementById("I_two").value == "2"){
+    points++;
+  } else {
+    wrong.push("I question 2 Wrong . Correct Ans: 2");
+  }
+  if(document.getElementById("tfans1").value == "True"){
+    points++;
+  } else {
+    wrong.push("II question 1 Wrong . Correct Ans: True");
+  }
+  if(document.getElementById("tfans2").value == "False"){
+    points++;
+  } else {
+    wrong.push("II question 2 Wrong . Correct Ans: False");
+  }
+  if(document.getElementById("tfans3").value == "False"){
+    points++;
+  } else {
+    wrong.push("II question 3 Wrong . Correct Ans: False");
+  }
+  window.alert(points + " / 5");
+  window.alert(wrong);
+  points = 0;
 }
